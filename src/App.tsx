cbf51ae6a73c0efab4906d9da1426e31d5c3d876
Antonis Margaronis <antonymarg@gmail.com>
2025-09-06 15:31:55 +0300

    Create basic App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { pokemonList } from './lib/pokemonList'
+
 function App() {
   return (
-    <>
-      <h1>React Pokemon Training</h1>
-      <p>Just for you!</p>
-    </>
+    <div className="flex min-h-screen w-full flex-col bg-gray-50">
+      <h1 className="text-2xl font-bold p-4">Welcome to Pokemon Training</h1>
+      <main className="flex-1 p-4 sm:p-6">
+        Your starter is: {pokemonList[0].name}
+      </main>
+    </div>
   )
 }
 
