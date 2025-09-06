5703854a5a12dbdc5867604eaf260d5a4ff3bef4
Antonis Margaronis <antonymarg@gmail.com>
2025-09-06 16:09:19 +0300

    Improve NoPokemonMessage

--- src/components/layout/NoPokemonMessage.tsx
+++ src/components/layout/NoPokemonMessage.tsx
@@ -1,7 +1,14 @@
 export function NoPokemonMessage() {
   return (
-    <div>
-      <h2>No Pokemon caught yet!</h2>
+    <div className="flex flex-1 items-center justify-center h-full w-full">
+      <div className="text-center">
+        <h2 className="text-6xl font-bold text-gray-600 mb-4">
+          No Pokemon caught yet!
+        </h2>
+        <p className="text-xl text-gray-500">
+          Start your Pokemon journey by catching your first Pokemon!
+        </p>
+      </div>
     </div>
   )
 }
